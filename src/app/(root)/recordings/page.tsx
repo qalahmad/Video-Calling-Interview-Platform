b8ccf6ba89fa159a8f4fc7616a9a486635966f92
Moc01 <lwa124@sfu.ca>
2025-05-18 21:37:44 -0700

    restructure the project

---
+++ src/app/(root)/recordings/page.tsx
@@ -0,0 +1,9 @@
+function RecordingsPage() {
+    return (
+        <div>
+            <h1>Recordings Page</h1>
+        </div>
+    )
+}
+
+export default RecordingsPage;
---
+++ src/app/(root)/schedule/page.tsx
@@ -0,0 +1,9 @@
+function SchedulePage() {
+    return (
+        <div>
+            <h1>Schedule Page</h1>
+        </div>
+    )
+}
+
+export default SchedulePage;
